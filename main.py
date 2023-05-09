63ee55cb11898af9cf9da9691b305a4133684553
Encryptunkh <Encryptunkh@gmail.com>
2023-05-09 14:16:58 +0300

    v1.2.1

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import numpy as np
 
 
-class Cell:
+class Cell():
     def __init__(self, x, y, is_mined):
         self.x = x
         self.y = y
         self.mines_num = 0
         self.is_mined = is_mined
+        self.is_shown = 0
+        self.is_marked = 0
 
     def __str__(self):
+        if self.is_marked == 1:
+            return '^'
+        if self.is_shown == 0:
+            return '?'
         if not self.is_mined:
             return str(self.mines_num)
         else:
             return '*'
 
     def __repr__(self):
+        if self.is_marked == 1:
+            return '^'
+        if self.is_shown == 0:
+            return '?'
         if not self.is_mined:
             return str(self.mines_num)
         else:
             return '*'
 
 
-class Field:
+class Field():
     def __init__(self, x_size, y_size, mines):
+        np.random.seed(1)
         self.x_size = x_size
         self.y_size = y_size
         self.mines_num = mines
@@ -43,24 +54,45 @@ class Field:
                 nghbs = self.get_nghbs(i, j)
                 temp = 0
                 for nghb in nghbs:
-                    temp += self.field_arr[nghb].is_mined
-                self.field_arr[i][j].mines_num = temp
+                    temp += nghb.is_mined
+                self.field_arr[j][i].mines_num = temp
 
     def __str__(self):
         return str(self.field_arr)
 
-    def get_nghbs(self, y, x):
+    def get_nghbs(self, x, y):
         nghbs = []
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if i == j == 0:
                     continue
-                if y + i in range(0, self.y_size) and x + j in range(0, self.x_size):
-                    nghbs.append((y + i, x + j))
+                if y + i > -1 and y + i < self.y_size and x + j > -1 and x + j < self.x_size:
+                    if self.field_arr[y + i][x + j].is_shown == 0:
+                        nghbs.append(self.field_arr[y + i][x + j])
         return nghbs
 
+    def reveal(self, x, y):
+        if self.field_arr[y][x].is_mined == 1:
+            print('game_over')
+            return
+        self.field_arr[y][x].is_shown = 1
+        nghbs = self.get_nghbs(x, y)
+        for nghb in nghbs:
+            nghb.is_shown = 1
+            if nghb.mines_num == 0:
+                self.reveal(nghb.x, nghb.y)
 
-if __name__ == '__main__':
-    fld = Field(6, 6, 10)
+
+fld = Field(9, 9, 10)
+while (True):
     print(fld)
-    print(fld.get_nghbs(5, 5))
+    inp = int(input())
+    if inp == 0:
+        break
+    if inp == 1:
+        xy = input().split(' ')
+        fld.reveal(int(xy[0]), int(xy[1]))
+    if inp == 2:
+        xy = input().split(' ')
+        fld.field_arr[int(xy[1])][int(xy[0])].is_marked = 1
+
