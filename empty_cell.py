efeee68a5362cfdd7da670c65ea10315e941d50b
Maxim <bushuev4329@gmail.com>
2023-05-09 15:48:24 +0300

    v1.4

--- empty_cell.py
+++ empty_cell.py
@@ -11,11 +11,45 @@ class Cells(Sprite):
         self.screen = eg_game.screen
         self.settings = eg_game.settings
 
+        #
+        self.is_mined = 0
+        self.num_mines = 0
+        self.is_shown = False
+
         # Каждая новая ячейка появляется в левом верхнем углу экрана.
         self.rect = pygame.Rect(0, 0, self.settings.cell_width, self.settings.cell_height)
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
+        # Настройки шрифта для вывода цифры.
+        self.text_color = (0, 0, 0)  # (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
     def draw_cell(self, color):
         """Размещение ячейки на экране."""
         self.screen.fill(color, self.rect)
+        if self.is_shown:
+            self.show_score()
+
+    def prep_info(self):
+        """Преобразует текущий счет в графическое изображение."""
+        if self.is_mined == 1:
+            score_str = '*'
+        else:
+            score_str = str(self.num_mines)
+        self.score_image = self.font.render(score_str, True,
+                                            self.text_color, self.settings.cell_color)
+
+        # Вывод счета выше кноки Play.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.center = self.rect.center
+        # self.score_rect.centery -= 50
+
+    def show_score(self):
+        """Вывод счета во время игры."""
+
+        self.prep_info()
+        self.screen.blit(self.score_image, self.score_rect)
+
+        # self.score_rect.bottom = 3 * (self.settings.cell_indent + self.settings.cell_height)
+        # self.screen.blit(self.score_image, self.score_rect)
--- game.py
+++ game.py
@@ -1,15 +1,17 @@
 import time
 
+import numpy as np
 import pygame
 import sys
 
 from empty_cell import Cells
-from opened_cells import OpenedCells
+# from opened_cells import OpenedCells
 from settings import Settings
 from minesscore import MinesScore
 from game_stats import GameStats
 from button import Button
 # from questions import Question
+from main import Field
 
 
 class MineSwiper:
@@ -28,13 +30,12 @@ class MineSwiper:
         # и статистики результатов.
         self.stats = GameStats(self)
         self.ms = MinesScore(self)
-        #
-        # self.questions = Question(self)
+
         self.cells = pygame.sprite.Group()
         self.opened_cells = pygame.sprite.Group()
         self._create_grid_of_cells()
-        #
-        # # Создание кнопки PLay и кнопки выхода из уровня
+
+        # Создание кнопки PLay и кнопки выхода из уровня
         self.play_button = Button(self, self.settings.start_button, "Play")
         self.quit_level_button = Button(self, self.settings.start_button, "Quit level")
         self.quit_level_button.rect.bottom = 70   # 8 * (self.settings.cell_height + self.settings.cell_indent)
@@ -90,6 +91,9 @@ class MineSwiper:
     def _start_game(self):
         """Запускает новую игру при нажатии P или кнопки Play."""
         if not self.stats.game_active:
+            # Создание поля
+            self.field = Field(self.settings.x_cells, self.settings.y_cells, self.settings.mines_limit)
+
             # Сброс игровой статистики.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -101,6 +105,37 @@ class MineSwiper:
 
             # Создание ячеек с вопросами.
             self._create_grid_of_cells()
+            self._set_first_mines()
+
+    def _set_first_mines(self):
+        """Задание мин на начальном поле."""
+        field_arr = np.zeros((self.settings.x_cells * self.settings.y_cells), dtype=int)
+
+        for i in range(self.settings.mines_limit):
+            field_arr[i] = 1
+        np.random.shuffle(field_arr)
+
+        for num, cell in enumerate(self.cells):
+            cell.is_mined = field_arr[num]
+
+        for cell in self.cells:
+            self._get_neighbors(cell)
+
+            for neigh_cell in cell.neighs:
+                cell.num_mines += neigh_cell.is_mined
+
+    def _get_neighbors(self, cell):
+        """Нахождение соседей клетки."""
+        cell_x, cell_y = convert(cell.rect[0], cell.rect[1])
+        cell.neighs = []
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                if x == y == 0:
+                    continue
+                if cell_x + x in range(0, self.settings.x_cells) and cell_y + y in range(0, self.settings.y_cells):
+                    xy = cell_x + x + cell_y + y
+                    if not self.cells.sprites()[xy].is_shown:
+                        cell.neighs.append(self.cells.sprites()[xy])
 
     def _check_cells_button(self, mouse_pos):
         """Проверка нажатия на ячейку с вопросом."""
@@ -108,7 +143,7 @@ class MineSwiper:
             if cell.rect.collidepoint(mouse_pos):
                 # self.stats.question_active = True
                 self._create_opened_cell(cell)
-                self.cells.remove(cell)
+                # self.cells.remove(cell)
                 # self.questions.get_new_question()
                 break
 
@@ -149,10 +184,11 @@ class MineSwiper:
 
     def _create_opened_cell(self, cell):
         """Создание пустой ячейки и размещение ее в ряду."""
-        opened_cell = OpenedCells(self)
-        opened_cell.rect.x = cell.rect.x
-        opened_cell.rect.y = cell.rect.y
-        self.opened_cells.add(opened_cell)
+        # opened_cell = OpenedCells(self)
+        # opened_cell.rect.x = cell.rect.x
+        # opened_cell.rect.y = cell.rect.y
+        # self.opened_cells.add(opened_cell)
+        cell.is_shown = True
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
@@ -177,6 +213,18 @@ class MineSwiper:
 
         pygame.display.flip()
 
+
+def convert(x, y, forw=True):
+    if forw:
+        new_x = int((x - 170) / 50)
+        new_y = int((y - 310) / 50)
+
+        return new_x, new_y
+    else:
+        new_x = x * 50 + 170
+        new_y = y * 50 + 310
+
+        return new_x + new_y
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры
